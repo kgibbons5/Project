596d94be2147bddd61f80a944d003886a55e9573
Poraich <Poraich@PT3000.nuigalway.ie>
2014-01-23 15:43:33 +0000

    Working Login

--- src/java/enterprise/web_jpa_war/servlet/LoginServlet.java
+++ src/java/enterprise/web_jpa_war/servlet/LoginServlet.java
@@ -6,8 +6,13 @@
 
 package enterprise.web_jpa_war.servlet;
 
+import enterprise.web_jpa_war.entity.Customer;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -31,30 +36,49 @@ public class LoginServlet extends HttpServlet {
      * @throws ServletException if a servlet-specific error occurs
      * @throws IOException if an I/O error occurs
      */
+ 
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
+            throws ServletException, IOException, ClassNotFoundException {
         response.setContentType("text/html;charset=UTF-8");
         PrintWriter out = response.getWriter();
+       
+        
         try {
             /* TODO output your page here. You may use following sample code. */
+         Class.forName("com.mysql.jdbc.Driver");
+        Connection con= (Connection) DriverManager.getConnection("jdbc:mysql://danu6.it.nuigalway.ie/mydb1253","mydb1253gk","lu9syq");
+        
+        String sql="Select * from Customer";
+        Statement stmt=con.createStatement();
+        ResultSet rs = stmt.executeQuery(sql);
+        
             String inputId         = (String) request.getParameter("id");
             String inputPassword   = (String) request.getParameter("password");
-            
-            if((inputId == null || inputId.equals("")||(inputPassword == null || inputPassword.equals(""))))
+                int tmp=0;
+            while(rs.next()) 
             {
-                RequestDispatcher rd = getServletContext().getRequestDispatcher("/logFail.jsp");
-                rd.include(request, response);
-            }
-            else 
-            {    
-                 RequestDispatcher pd = getServletContext().getRequestDispatcher("/logSuccess.jsp");
+            String uname=rs.getString("cID");
+//Username is the coloumn name in the database table 
+            String pass=rs.getString("password");
+            if ((inputId.equals(uname)) && (inputPassword.equals(pass))) 
+            {
+                RequestDispatcher pd = getServletContext().getRequestDispatcher("/logSuccess.jsp");
                  pd.include(request, response);
-                        
+                   tmp++;
+                }
             }
+            if (tmp==0) 
+            {
+             RequestDispatcher rd = getServletContext().getRequestDispatcher("/logFail.jsp");
+                rd.include(request, response);
+               }
             
-        } finally {
+        } catch (Exception e){}
+        
+finally {
             out.close();
         }
+        
     }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
@@ -69,7 +93,11 @@ public class LoginServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+        try {
+            processRequest(request, response);
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(LoginServlet.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 
     /**
@@ -83,7 +111,11 @@ public class LoginServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+        try {
+            processRequest(request, response);
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(LoginServlet.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 
     /**
